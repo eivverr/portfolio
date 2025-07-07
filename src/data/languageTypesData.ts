7dc161146d7b2bcedf50254def4939f636a59b82
Loris <loris.weier57@gmail.com>
2025-07-07 19:54:24 +0200

    Add projects

--- src/data/languageTypesData.ts
+++ src/data/languageTypesData.ts
@@ -5,4 +5,5 @@ export const languages = {
     javascript: { name: 'Javascript', shortName: 'JS', color: '#f7df1e' },
     php: { name: 'Php', color: '#777bb3' },
     unity: { name: 'Unity', color: '#000000' },
+    vue: { name: 'Vuejs', shortName: 'Vue', color: '#41b883' },
 } as const satisfies Record<string, LanguageType>
